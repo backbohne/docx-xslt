df37c0fa889d80899631a4e7c5a4dbf48c6e3ebc
Frank Bohnsack <fbo@deep.ch>
2015-02-26 11:48:16 +0100

    change tests

--- tests/test.py
+++ tests/test.py
@@ -5,30 +5,10 @@ sys.path.insert(0, 'docxxslt')
 from lxml import etree
 from docxxslt import DocxXsltTemplate, utils
 
-filename = "tests/test.docx"
-context = etree.XML("""
-<data>
-    <name>Testkunde ABC</name>
-       <products>
-           <product>
-               <name>Produkt 1</name>
-               <price>100</price>
-           </product>
-           <product>
-               <name>Produkt 2</name>
-               <price>150</price>
-        </product>
-           <product>
-               <name>Produkt 3</name>
-               <price>200</price>
-        </product>
-           <product>
-               <name>Produkt 4</name>
-               <price>500</price>
-        </product>
-    </products>
-</data>
-""")
+docx_in = "tests/test.docx"
+docx_out = "xtest.docx"
+xml_in = "tests/test.xml"
+context = etree.parse(xml_in)
 
-template = DocxXsltTemplate(filename)
-template.save(filename="x%s" % os.path.basename(filename), context=context, logger=utils.get_logger())
+template = DocxXsltTemplate(docx_in)
+template.save(filename=docx_out, context=context, logger=utils.get_logger())
